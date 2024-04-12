2ec3ffc4856d0c53636ae32d7f6e9e1c8a555c27
Stana Radu <stana.radu@gmail.com>
2024-04-12 14:13:24 +0300

    changes for deploy

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://smartbrain-api-7u95.onrender.com/imageurl',{
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -104,7 +104,7 @@ class App extends Component {
       .then(response => response.json())
       .then(result  => {
         if(result){
-          fetch('http://localhost:3000/image',{
+          fetch('https://smartbrain-api-7u95.onrender.com/image',{
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
