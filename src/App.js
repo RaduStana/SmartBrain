fe950a12648cab3da7237595694c064514871f71
Stana Radu <stana.radu@gmail.com>
2024-04-12 17:24:09 +0300

    Changed response from ClarifaiAPI

--- src/App.js
+++ src/App.js
@@ -71,8 +71,7 @@ class App extends Component {
     });
   }    
 
-  calculateFaceLocation = (data) => {
-      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  calculateFaceLocation = (clarifaiFace) => {
       const image = document.getElementById('inputimage');
       const width = Number(image.width);
       const height = Number(image.height);
